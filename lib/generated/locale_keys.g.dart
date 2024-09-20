41f7ed17a10e35234f85cef252a947ecc29795fd
Yehya <yehyaelzaeem7@gmail.com>
2024-09-20 17:31:12 +0300

    onBoarding

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,17 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const onboarding1 = 'onboarding1';
+  static const onboarding2 = 'onboarding2';
+  static const onboarding3 = 'onboarding3';
+  static const title1 = 'title1';
+  static const title2 = 'title2';
+  static const title3 = 'title3';
+  static const skip = 'skip';
+  static const next = 'next';
+  static const getStarted = 'getStarted';
+  static const en = 'en';
+  static const ar = 'ar';
+  static const changeLanguage = 'changeLanguage';
+
+}
